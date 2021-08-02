8f5ecae6f62c23297a073d717e32b7c0898979de
Zhou Weng <88307843+zweng5@users.noreply.github.com>
2021-08-02 15:18:01 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,82 @@
+function run() {
+  var results;
+  $.when(
+      $.ajax({
+          type: "GET",
+          url: './stats.json',
+          contentType: "application/json; charset=utf-8",
+          dataType: "text",
+          success: function(data) {
+              results = JSON.parse(data);
+          },
+          error: function (xhr, textStatus, errorThrown) {
+              console.log("Error getting the stats", newTS, xht, textStatus, errorThrown);
+          }
+      }),
+  ).then(function() {
+    //# show stats
+    //console.log(results);
+    div = $("<div>").html(parseInt(results["stats"]["days_since_last_local_covid"])
+      + "<div class=\"small\">days since last local Covid-19 case in HK!</div>");
+    div.addClass("number");
+    $("#container").append(div);
+    
+    buffer = '';
+    if(results["stats"]["number_of_too_recent_local_cases"] == 0) {
+      //buffer = "No other recent local cases that could be reclassified";
+    } else {
+      s = (results["stats"]["number_of_too_recent_local_cases"] > 1) ? 's' : '';
+      buffer = parseInt(results["stats"]["number_of_too_recent_local_cases"])
+      + "<div class=\"small\">case"+s+" within the last " +
+          parseInt(results["stats"]["days_waited_for_classification_confirmation"]) +
+          "days, to be confirmed as local or not</div>";
+      div = $("<div>").html(buffer);
+      div.addClass("number");
+      $("#container").append(div);
+    }
+
+    buffer = '';    
+    if(results["stats"]["days_since_last_local_covid"] == 
+       results["stats"]["longest_no_local_case"]) {
+      buffer = "This is currently the longest period of time without local cases since the start of the pandemic!";
+    } else {
+      buffer = "The longest period of time without local cases was " +
+                results["stats"]["days_since_last_local_covid"] +
+               "days";
+    }
+    div = $("<div>").html(buffer);
+    div.addClass("info");
+    $("#container").append(div);
+
+    div = $("<div>").html(parseInt(results["stats"]["number_of_deceased"])
+      + "<div class=\"small\">deceased victims of Covid-19 in HK since the beginning of the pandemic</div>");
+    div.addClass("number");
+    $("#container").append(div);
+    
+    div = $("<div>").html(Math.round(parseFloat(results["stats"]["mean_age_deceased"])*10, 1)/10 +
+      "<div class=\"small\">years old is the average age of the deceased victims of Covid-19 in HK (the median age is " +
+      Math.round(parseFloat(results["stats"]["median_age_deceased"])*10, 1)/10 +
+      " years old)</div>");
+    div.addClass("number");
+    $("#container").append(div);
+    
+    buffer = parseInt(results["stats"]["number_hospitalised"]) +
+      "<div class=\"small\">people are currently hospitalised ";
+    n = parseInt(results["stats"]["hospitalised_local_cases"]);
+    if(n == 0) {
+      buffer += "(all are imported cases)";
+    } else {
+      buffer += "(" + n +
+      " are local cases)";
+    }
+    buffer += "</div>";
+    div = $("<div>").html(buffer);
+    div.addClass("number");
+    $("#container").append(div);
+
+    div = $("<div>").html(parseInt(results["stats"]["longest_currently_hospitalised"]) +
+      "<div class=\"small\">days is the longest hospitalisation among current patients</div>");
+    div.addClass("number");
+    $("#container").append(div);
+  });
+}
